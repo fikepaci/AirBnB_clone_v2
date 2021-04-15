28cc746ca91cbf80ec9801802458809c860e83ac
fikepaci <pacifiquemachine@gmail.com>
2021-04-15 13:33:29 +0200

    add files

--- models/place.py
+++ models/place.py
@@ -28,7 +28,7 @@ class Place(BaseModel, Base):
     """Represents a Place for a MySQL database.
 
     Inherits from SQLAlchemy Base and links to the MySQL table places.
-    
+
     Attributes:
         __tablename__ (str): The name of the MySQL table to store places.
         city_id (sqlalchemy String): The place's city id.
